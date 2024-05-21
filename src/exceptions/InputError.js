d792328bab91ad88beab45888e884dcfe7440217
mutiaistiqomah23 <mutiaistiqomah@upi.edu>
2024-05-21 18:31:22 +0700

    first commit

---
+++ src/exceptions/InputError.js
@@ -0,0 +1,12 @@
+const ClientError = require("../exceptions/ClientError");
+ 
+class InputError extends ClientError {
+    constructor(message) {
+        super(message);
+        this.name = 'InputError';
+    }
+}
+
+
+
+module.exports = InputError;
---
+++ src/server/routes.js
@@ -0,0 +1,20 @@
+const postPredictHandler = require('../server/handler');
+ 
+const routes = [
+  {
+    path: '/predict',
+    method: 'POST',
+    handler: postPredictHandler,
+    options: {
+      payload: {
+        /*Mengizinkan data berupa gambar*/
+        allow: 'multipart/form-data',
+        multipart: true
+      }
+    }
+  }
+]
+
+
+
+module.exports = routes;
